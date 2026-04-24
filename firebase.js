e058212c393459d2bb9bb4561bd4293682488ed5
LeshanaJr <49703740+LeshanaJr@users.noreply.github.com>
2026-04-24 07:55:59 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,14 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import {
+  getFirestore,
+  doc,
+  setDoc,
+  increment,
+  collection,
+  addDoc,
+  serverTimestamp
+} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
