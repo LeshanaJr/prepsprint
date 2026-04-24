734e251b72edabd5f284f483299a20f1e0157389
LeshanaJr <49703740+LeshanaJr@users.noreply.github.com>
2026-04-24 08:13:22 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -27,7 +27,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-const analytics = getAnalytics(app);
 
 function getGlobalUserId() {
 
