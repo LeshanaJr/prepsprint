4a83e8684dce6a97814e93e7b463e955125db585
LeshanaJr <49703740+LeshanaJr@users.noreply.github.com>
2026-04-25 20:47:25 -0400

    Delete questions/ap-gov/manifest.js

--- questions/ap-gov/manifest.js
+++
@@ -1,7 +0,0 @@
-window.questionManifest = [
-  "questions/ap-gov/unit-1.js",
-  "questions/ap-gov/unit-2.js",
-  "questions/ap-gov/unit-3.js",
-  "questions/ap-gov/unit-4.js",
-  "questions/ap-gov/unit-5.js"
-];
