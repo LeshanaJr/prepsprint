03c83e87758672d352c8d5e597ca2188e31265b6
LeshanaJr <49703740+LeshanaJr@users.noreply.github.com>
2026-04-25 21:29:50 -0400

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -1,16 +1,16 @@
 window.UNIT_MANIFEST = {
   apGov: [
-    "units/ap-gov/unit-1.js"
-    "units/ap-gov/unit-2.js"
-"units/ap-gov/unit-3.js"
-"units/ap-gov/unit-4.js"
-"units/ap-gov/unit-5.js"
+    "questions/ap-gov/unit-1.js",
+    "questions/ap-gov/unit-2.js",
+    "questions/ap-gov/unit-3.js",
+    "questions/ap-gov/unit-4.js",
+    "questions/ap-gov/unit-5.js"
   ],
 
   apJava: [
-    "units/ap-java/unit-1.js",
-    "units/ap-java/unit-2.js",
-    "units/ap-java/unit-3.js",
-    "units/ap-java/unit-4.js"
+    "questions/ap-java/unit-1.js",
+    "questions/ap-java/unit-2.js",
+    "questions/ap-java/unit-3.js",
+    "questions/ap-java/unit-4.js"
   ]
 };
