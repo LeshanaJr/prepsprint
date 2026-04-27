66190be9c4bd8d9cb89fa73ab78df2dfefedb404
LeshanaJr <49703740+LeshanaJr@users.noreply.github.com>
2026-04-27 19:44:18 -0400

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -12,5 +12,17 @@ window.UNIT_MANIFEST = {
     "questions/ap-java/unit-2.js",
     "questions/ap-java/unit-3.js",
     "questions/ap-java/unit-4.js"
+  ],
+
+  apChem: [
+    "questions/ap-chem/unit-1.js",
+    "questions/ap-chem/unit-2.js",
+    "questions/ap-chem/unit-3.js",
+    "questions/ap-chem/unit-4.js",
+    "questions/ap-chem/unit-5.js",
+    "questions/ap-chem/unit-6.js",
+    "questions/ap-chem/unit-7.js",
+    "questions/ap-chem/unit-8.js",
+    "questions/ap-chem/unit-9.js",
   ]
 };
