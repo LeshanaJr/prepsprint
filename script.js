50418776ff896174d210cd0c419842de22cc3bd3
LeshanaJr <49703740+LeshanaJr@users.noreply.github.com>
2026-04-30 10:16:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -233,8 +233,8 @@ function getSubjectDescription(subjectName) {
 function getSubjectIcon(subjectName) {
   if (subjectName === "AP Lang") return "📘";
   if (subjectName === "AP World" || subjectName === "AP Gov") return "🌍";
-  if (SubjectName === "AP Chem") return "🧪";
-  if (SubjectName === "AP Bio") return "🧬";
+  if (subjectName === "AP Chem") return "🧪";
+  if (subjectName === "AP Bio") return "🧬";
   return "📚";
 }
 
