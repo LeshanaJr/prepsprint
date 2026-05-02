62733f5393e0b336e7e7918f9deb508cb615105b
LeshanaJr <49703740+LeshanaJr@users.noreply.github.com>
2026-05-02 16:11:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -749,7 +749,13 @@ function showMorePage() {
     <div class="subject-card">
       <div class="subject-card-title">What is PrepSprint?</div>
       <p class="home-stats-text">
-        PrepSprint helps you study with AP-style practice, explanations, and subject-based review modes.
+        PrepSprint is a 100% free web app that was made to help people study and prepare for their AP exams. More subjects are actively getting added, so be on the lookout for them!\n\n
+        
+        PrepSprint currently supports passage-based questions, rapid-fire questions for memorizing FRQ concepts, and timed tests to simulate the real exams.\n\n
+
+        AP© is trademarked by CollegeBoard, who does not affiliate with or sponsor PrepSprint in any way.\n\n
+
+        App created by Warren Grant
       </p>
     </div>
 
