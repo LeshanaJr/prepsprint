8af083869eeaf150d1db0504da5a74c209373f3a
LeshanaJr <49703740+LeshanaJr@users.noreply.github.com>
2026-05-05 09:26:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1461,6 +1461,7 @@ try {
      buildSubject("AP Chem", "apChem"),
      buildSubject("AP Bio", "apBio"),
      buildSubject("AP Lang", "apLang"),
+     buildSubject("AP Lit", "apLit"),
      buildSubject("AP World", "apWorld"),
      buildSubject("AP Precalc", "apPrecalc"),
      buildSubject("AP Human Geography", "apHumanGeo"),
